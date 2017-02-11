2451c89b03c333e6bcad57b05471b2626ebb021d
pawel guzek <guzek.pwl@gmail.com>
2017-02-11 10:18:25 +0100

    Added basic version od managing satelite code

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,4 +2,10 @@ public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We will be Git experts, soon.");
     }
+	
+	
+	public void manageTheSatellite() {
+    // TODO implement
+}
+
 }
